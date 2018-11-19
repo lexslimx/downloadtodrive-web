8e38d43791114d8375977e33e4e8cba07b1749a8
Alex Kinyua <alex.km@live.com>
2018-11-20 00:05:58 +0400

    update api url

--- d2dcore/src/environments/environment.prod.ts
+++ d2dcore/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://downloadtodriveapi.azurewebsites.net/api/',
-  signalRServer: 'https://downloadtodriveapi.azurewebsites.net/api/chatHub',
-  loginUrl: 'https://downloadtodriveapi.azurewebsites.net/account/authenticate',
-  registrationIUrl: 'https://downloadtodriveapi.azurewebsites.net/register'
+  apiUrl: 'https://d2dcoreapi.azurewebsites.net/api/',
+  signalRServer: 'https://d2dcoreapi.azurewebsites.net/api/chatHub',
+  loginUrl: 'https://d2dcoreapi.azurewebsites.net/account/authenticate',
+  registrationIUrl: 'https://d2dcoreapi.azurewebsites.net/register'
 };
