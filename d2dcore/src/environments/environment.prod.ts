8878e2538ea8ce8615732df99b1892cf32c53c5f
alex.km@live.com <alex.km@live.com>
2018-11-20 11:15:13 +0400

    fixed hub api

--- d2dcore/src/environments/environment.prod.ts
+++ d2dcore/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://d2dcoreapi.azurewebsites.net/api/',
-  signalRServer: 'https://d2dcoreapi.azurewebsites.net/api/chatHub',
+  signalRServer: 'https://d2dcoreapi.azurewebsites.net/chatHub',
   loginUrl: 'https://d2dcoreapi.azurewebsites.net/account/authenticate',
   registrationIUrl: 'https://d2dcoreapi.azurewebsites.net/account/register'
 };
