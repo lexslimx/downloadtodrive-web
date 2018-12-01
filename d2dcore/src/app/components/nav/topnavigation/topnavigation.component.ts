be9d3f1f7e9c5ad04b4abc4d3f999ae94176a44e
Alex Kinyua <alex.km@live.com>
2018-12-01 20:39:31 +0400

    login/out functionality

--- d2dcore/src/app/components/nav/topnavigation/topnavigation.component.ts
+++ d2dcore/src/app/components/nav/topnavigation/topnavigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-topnavigation',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopnavigationComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private authService: AuthService, private router: Router) { }
+  currentAction = 'Sign In';
   ngOnInit() {
+    if (this.authService.isAuthenticated()) {
+      this.currentAction = 'Sign Out';
+    } else {
+      this.currentAction = 'Sign In';
+    }
   }
 
+  logIn() {
+    if (this.authService.isAuthenticated()) {
+      this.authService.logout();
+       this.router.navigate(['login']);
+       this.currentAction = 'Sign In';
+    } else {
+      this.router.navigate(['login']);
+      this.currentAction = 'Sign In';
+    }
+  }
 }
