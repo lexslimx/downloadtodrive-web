ed93961d066e2746fb47c3adf27220f04d850d65
Alex Maina <alex.km@live.com>
2019-07-16 14:07:26 +0400

    corrected api

--- d2dcore/src/environments/environment.prod.ts
+++ d2dcore/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://downloadtodriveapi.azurewebsites.net/api/',
-  signalRServer: 'https://downloadtodrivesr.service.signalr.net/chat',
+  signalRServer: 'https://downloadtodriveapi.azurewebsites.net/chat',
   loginUrl: 'https://d2dcoreapi.azurewebsites.net/account/authenticate',
   registrationIUrl: 'https://d2dcoreapi.azurewebsites.net/account/register',
   ytServiceUrl:'https://downloadtodriveapi.azurewebsites.net/api/Youtube'
