9265c65077f497f2c11e26c532bc1326e2d8329b
Alex Maina <alex.km@live.com>
2019-08-17 16:14:19 +0400

    fml! another typo

--- d2dcore/src/environments/environment.prod.ts
+++ d2dcore/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://downloadtodriveapi.azurewebsites.net/api/',
   signalRServer: 'https://downloadtodriveapi.azurewebsites.net/chat',
-  loginUrl: 'https://downloadtodriveapi.azurewebsites.net/account/api/account/login',
+  loginUrl: 'https://downloadtodriveapi.azurewebsites.net/api/account/login',
   registrationIUrl: 'https://downloadtodriveapi.azurewebsites.net/api/account/register',
   ytServiceUrl:'https://downloadtodriveapi.azurewebsites.net/api/Youtube'
 };
